fd447666a34fff923c1e14c2d7ffa29977af80d5
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-08-03 12:53:24 -0700

    Package infrastructure

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang setup/infotab
+(define collection 'multi)
+(define deps '(
+	       ;; these are what the package system tells me I need:
+	       ))
+(define build-deps '(
+		     ;; these are what the package system tells me I need:
+		     ))
